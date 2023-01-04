ec8702604e940a37c0a60bb9ec066c8c63403c34
ThMenezes <thita.menezes24@gmail.com>
2023-01-04 15:35:30 -0300

    ativando classe hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,8 @@ const elementoPais = document.querySelector('#pais');
 const elementoUmidade = document.querySelector('#umidade span');
 const elementoVento = document.querySelector('#vento span');
 
+const containerTempo = document.querySelector('#weather-data')
+
 // obter clima da cidade
 const obterMeteorologia = async(cidade) => {
     const apiTempoURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`
@@ -19,18 +21,38 @@ const obterMeteorologia = async(cidade) => {
     const resposta = await fetch(apiTempoURL)
     const data = await resposta.json()
 
-    console.log(data)
-}
+    return data
+};
 
-//função mostrar cidade
-const mostrarMeteorologia = (cidade) => {
-    obterMeteorologia(cidade)
-}
+//função mostrar cidade e alterar informações na tela
+const mostrarMeteorologia = async (cidade) => {
+    const data = await obterMeteorologia (cidade);
+
+    elementoCidade.innerText = data.name;
+    elementoTemperatura.innerText = parseInt(data.main.temp);
+    elementoDescricao.innerText = data.weather[0].description;
+    elementoIcone.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    elementoPais.setAttribute("src", apiPaisURL + data.sys.country);
+    elementoUmidade.innerText = `${data.main.humidity}%`;
+    elementoVento.innerText = `${data.wind.speed}km/h`;
+
+    // mostrar o container da função mostrarMeteorologia
+    containerTempo.classList.remove("hide")
+};
 
 botãoSearch.addEventListener("click", (e) => {
     e.preventDefault();
 
     const cidade = inputCidade.value;
     mostrarMeteorologia(cidade)
-})
+});
+ 
+// evento input com enter
+inputCidade.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const cidade = e.target.value
+
+        mostrarMeteorologia(cidade);
+    }
+});
 
